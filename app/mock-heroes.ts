7d7a0e60ee0dd1f7ff7a2b4415d7b648d9990902
Mahesh Jethva <mjethvar8@gmail.com>
2016-09-04 15:16:15 -0400

    - moved the heroes array to "app/mock-heroes.ts" - modified the hero.service.ts - modified the app.component.ts

---
+++ app/mock-heroes.ts
@@ -0,0 +1,12 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Mahltron' },
+  { id: 12, name: 'Matt L' },
+  { id: 13, name: 'Matt M' },
+  { id: 14, name: 'Julie R' },
+  { id: 15, name: 'Mike Z' },
+  { id: 16, name: 'Bobby F' },
+  { id: 17, name: 'Andrew F' },
+  { id: 18, name: 'Natalia V' }
+];
